9cfaa90d781e23623b10aeb8bfa8e166ac0b4605
fukupug <hiromu1996166@gmail.com>
2021-08-13 07:28:14 +0900

    Style User List Item

--- user-management/src/component/user-list/UserListItem.js
+++ user-management/src/component/user-list/UserListItem.js
@@ -3,7 +3,7 @@ import styles from "./UserListItem.module.css";
 const UserListItem = (props) => {
   const user = props.user;
   const content = user.name + " (" + user.age + "years old)";
-  return <li className={styles['user-list-item']}>{content}</li>;
+  return <li className={styles["user-list-item"]}>{content}</li>;
 };
 
 export default UserListItem;
