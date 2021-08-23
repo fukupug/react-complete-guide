04e44a2b28cf7599b22c8ca340ad9c9809c376a6
fukupug <hiromu1996166@gmail.com>
2021-08-24 08:45:25 +0900

    Create Meals component

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Header from "./componens/layout/Header";
+import Meals from "./componens/meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
