322f5424192f227fca5d0ae7f7fe613485662718
fukupug <hiromu1996166@gmail.com>
2021-08-24 18:47:52 +0900

    Create cart component

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,11 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import Cart from "./componens/cart/Cart";
 import Header from "./componens/layout/Header";
 import Meals from "./componens/meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
