0b413280373773b835b924ce0110796a25ba8ef8
fukupug <hiromu1996166@gmail.com>
2021-08-27 20:53:04 +0900

    Implement to look at child component Re-Evaluation

---
+++ look-behind-scenes-of-react/src/components/Demo/DemoOutput.js
@@ -0,0 +1,8 @@
+import MyParagraph from "./MyParagraph";
+
+const DemoOutput = (props) => {
+  console.log("DEMO OUTPUT RUNNING");
+  return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
+};
+
+export default DemoOutput;
---
+++ look-behind-scenes-of-react/src/components/Demo/MyParagraph.js
@@ -0,0 +1,6 @@
+const MyParagraph = (props) => {
+  console.log("MY PARAGRAPH RUNNING");
+  return <p>{props.chidlrem}</p>;
+};
+
+export default MyParagraph;
